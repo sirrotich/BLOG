deb9a8f8288b99332b4e76dfb5a9253ed9930cab
sirrotich <rotichtitus12@gmail.com>
2019-04-28 10:32:27 +0300

    define a function that takes the configurations

--- app/__init__.py
+++ app/__init__.py
@@ -7,3 +7,34 @@ from flask_uploads import UploadSet,configure_uploads,IMAGES
 from flask_mail import Mail
 from flask_babelex import Babel
 from flask_simplemde import SimpleMDE
+
+def create_app(config_name):
+
+
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    # configure UploadSet
+    configure_uploads(app,photos)
+
+    # Initializing flask extensions
+    Bootstrap(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    babel = Babel(app)
+    simple.init_app(app)
+
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+
+
+    return app
